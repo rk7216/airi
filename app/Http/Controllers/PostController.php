c3e2b8897251c4c2a5a3bed5c771c042f756c2bb
rk7216 <zlc_0t46b82k_jagm8_rei@yahoo.co.jp>
2024-02-26 11:14:58 +0000

    8-4 all end

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest; // useする
 use App\Models\Post;
 
 class PostController extends Controller
@@ -26,7 +27,7 @@ class PostController extends Controller
     {
         return view('posts/create');
     }
-    public function store(Request $request, Post $post)
+    public function store(PostRequest $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
