fdeb6d6d06d7c5e64a7bc97fbb77f547502bf3d7
rk7216 <zlc_0t46b82k_jagm8_rei@yahoo.co.jp>
2024-02-28 10:42:03 +0000

    8-6 end

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-         return view('posts.index')->with(['posts' => $post->getPaginateBylimit(1)]);
+         return view('posts.index')->with(['posts' => $post->getPaginateBylimit(5)]);
     }
         /**
      * 特定IDのpostを表示する
@@ -36,5 +36,19 @@ class PostController extends Controller
         $post->fill($input->save());
         return redirect('/posts/' . $post->id);
     */
+    }
+    public function edit(Post $post){
+        return view('posts/edit')->with(['post' => $post]);
+    }
+
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    public function delete(Post $post){
+         $post->delete();
+         return redirect('/');
     }
 }
